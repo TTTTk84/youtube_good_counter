2e07e93a40b9dbb3c6af92d6dc4112df10d1dc8d
Tk84 <bjjt1290058@gn.iwasaki.ac.jp>
2021-01-01 21:55:28 +0900

    [Add] good_counter

---
+++ json.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+func err_io(err error){
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s", err)
+	}
+}
+
+
+func (c *goodHandler) json_parse(r *http.Request) map[string]interface{}{
+	length, err := strconv.Atoi(r.Header.Get("Content-Length"))
+	err_io(err)
+
+	body := make([]byte, length)
+	length, _ = r.Body.Read(body)
+
+	var jsonBody map[string]interface{}
+	err = json.Unmarshal(body[:length], &jsonBody)
+	err_io(err)
+
+	return jsonBody
+}
+
+
+type discord struct {
+	Content string `json:"content"`
+}
+
+func (j *jobTicker) outputMessage(message_array *[]map[string]interface{}) {
+	// 入れる文字列を作成
+	content := fmt.Sprintf("**%d月%d日のいいね数 :** %d\n",
+	time.Now().Month(), time.Now().Day(), len(*message_array))
+	for _, mes := range *message_array {
+		content += fmt.Sprintf("いいねした動画 : **%s**   %s\n", mes["Title"], mes["Url"])
+	}
+
+
+	url := os.Getenv("DISCORD_WEBHOOK")
+	discord := &discord{}
+	discord.Content = content
+
+	// リクエストを作成
+	req_json, _ := json.Marshal(discord)
+	resp, err := http.NewRequest("POST", url, bytes.NewReader(req_json))
+	err_io(err)
+	resp.Header.Set("Content-Type","application/json")
+
+	client := new(http.Client)
+	_, err = client.Do(resp) // 送信
+	err_io(err)
+
+	*message_array = nil
+}
--- main.go
+++ main.go
@@ -1,27 +1,58 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
+type goodHandler struct {
+		msg   chan map[string]interface{}
+}
 
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatal("$PORT must be set")
+		log.Fatal("$PORT が設定されていません")
 	}
-	fmt.Println("デプロイテスト")
 
-	http.HandleFunc("/hello", hello)
+	good := &goodHandler{msg: make(chan map[string]interface{}, 10),}
+	job	 := &jobTicker{good: good}
+	go job.run()
+
+	http.Handle("/good", good)
 	if err := http.ListenAndServe(":" + port, nil); err != nil{
 		log.Fatal(err)
 	}
 }
 
-func hello(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("<h1>hello world</h1>"))
+func (c *goodHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		pass := md5_hash(os.Getenv("API_PASS"))
+
+		json := c.json_parse(r)
+		if pass == json["Pass"] {
+			c.msg <- json
+		}else {
+			fmt.Println("passwordが間違っています: ", json["Pass"])
+		}
+
+	}
+
+}
+
+// md5でハッシュ化する
+func md5_hash(pass string) string{
+	md5_pass := []byte(pass)
+
+	for i:=0; i<100; i++ {
+		temp := md5.Sum(md5_pass)
+		md5_pass = temp[:16]
+	}
+
+	return hex.EncodeToString(md5_pass)
 }

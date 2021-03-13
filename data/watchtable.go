77a433412ddda94931915fd0bf3583ec6ccc42b0
Tk84 <bjjt1290058@gn.iwasaki.ac.jp>
2021-03-13 16:48:19 +0900

    [Add] GetAll DeleteAll

--- data/watchtable.go
+++ data/watchtable.go
@@ -8,7 +8,7 @@ import (
 
 type Watchtable struct {
 	Title 	string
-	User  	string
+	Url  	string
 	LikedAt	string
 }
 
@@ -18,6 +18,48 @@ func (wt *Watchtable) CreateWatchTable(r *http.Request) (err error){
 	body := make([]byte, length)
 	length, _ = r.Body.Read(body)
 	err = json.Unmarshal(body[:length], &wt)
+	if err != nil {
+		return err
+	}
 
+	statement := "insert into watchtables (Title, User, LikedAt) values ($1, $2, $3) Title, Url, LikedAt"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	stmt.QueryRow(wt.Title,wt.Url,wt.LikedAt)
+
+	return err
+}
+
+func GetAllWatchTables() (wts []Watchtable, err error){
+	statement := "select * from watchtables"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query()
+
+	for rows.Next() {
+		var wt Watchtable
+		err = rows.Scan(
+			&wt.Title,
+			&wt.Url,
+			&wt.LikedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		wts = append(wts, wt)
+	}
+
+	return wts, err
+}
+
+func DeleteAllWatchTables() (err error){
+	statement := "delete from watchtables"
+	_, err = Db.Exec(statement)
 	return err
 }
--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"net/http"
+	"os"
+
+	"github.com/TTTTk84/youtube_good_counter/data"
+)
 
 func main() {
+	env_load()
+	port := os.Getenv("PORT")
+	data.SqlConnect()
 
+
+	http.HandleFunc("/good", good)
+	http.HandleFunc("/post", post)
+	http.ListenAndServe(":" + port, nil)
 }

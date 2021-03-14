060581a1523612483e3e44effd8293fed4cdf727
Tk84 <bjjt1290058@gn.iwasaki.ac.jp>
2021-03-15 00:58:59 +0900

    [Fix]

--- data/data.go
+++ data/data.go
@@ -16,8 +16,10 @@ var Db *sql.DB
 
 func SqlConnect(){
 	var err error
-	sourceName := fmt.Sprintf("dbname=%s sslmode=disable user=%s password=%s",
-										os.Getenv("DB_NAME"),os.Getenv("DB_USERNAME"),os.Getenv("DB_PASSWORD"))
+	// sourceName := fmt.Sprintf("dbname=%s sslmode=disable user=%s password=%s",
+	// 									os.Getenv("DB_NAME"),os.Getenv("DB_USERNAME"),os.Getenv("DB_PASSWORD"))
+	sourceName := fmt.Sprintf("%s",
+	 									os.Getenv("DATABASE_URL"))
 	Db, err = sql.Open("postgres", sourceName)
 	if err != nil{
 		log.Fatal(err)
